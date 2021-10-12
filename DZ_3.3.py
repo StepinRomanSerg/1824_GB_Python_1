9ebab01b0da1f50dabd5161a42ede09ed1a1b4c0
StepinRomanSerg <91904675+StepinRomanSerg@users.noreply.github.com>
2021-10-12 19:25:49 +0300

    Add files via upload

---
+++ DZ_3.3.py
@@ -0,0 +1,13 @@
+#3.Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь,
+# в котором ключи — первые буквы имен, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы.
+
+def thesaurus(*names):
+    res = {}
+    for name in names:
+        key = name[0].capitalize()
+        if key not in res:
+            res[key] = []
+        res[key].append(name)
+    return res
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
